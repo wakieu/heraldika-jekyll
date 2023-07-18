353d9426152f5adf35e8e4295b529534cc953412
vwaki <viniwte@gmail.com>
2023-07-18 11:14:30 -0300

    Base website v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import './App.css';
+
+import mapPath from './heraldic_map_of_europe_by_jekyll_by_jekyllpng_dg2jdha-pre.jpg'
+
+const wheelOpt = {step: 1.0, smoothStep: 0.005};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <h2 style={{fontFamily: "Eras Demi ITC"}}>
+          Very cool map 😎👍 by <a className="App-link" href="https://twitter.com/Yunacel" target="_blank" rel="noopener noreferrer">Jekyll</a>
+        </h2>
+
+        <a className="button" href="https://www.etsy.com/shop/HeraldikaJekyll" target="_blank" rel="noopener noreferrer">
+          <button>Buy physical version</button>
         </a>
+
+        <div style={{border: '0.1rem solid red', background: 'white', maxWidth: '85%', height: 'auto', margin: '5% 0 5rem'}}>
+          <TransformWrapper maxScale={6} smooth={false} wheel={wheelOpt}>
+            <TransformComponent>
+              <img src={mapPath} alt="heraldic map of Europe" style={{maxWidth: '100%', height: 'auto', pointerEvents: 'none'}} />
+            </TransformComponent>
+          </TransformWrapper>
+        </div>
+
       </header>
     </div>
   );
