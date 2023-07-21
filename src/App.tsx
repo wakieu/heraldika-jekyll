98a5075cbafb9c8a44d172672a3c410272f997a8
vwaki <viniwte@gmail.com>
2023-07-21 17:03:34 -0300

    Remove webvitals and tests

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,25 @@ const wheelOpt = {step: 1.0, smoothStep: 0.005};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
-        <h2 style={{fontFamily: "Eras Demi ITC"}}>
+      <div style={{background: 'white', maxHeight: '85%', width: 'auto', justifyContent: 'center'}}>
+        <TransformWrapper maxScale={6} smooth={true} wheel={wheelOpt} centerOnInit={true}> 
+          <TransformComponent>
+            <img src={mapPath} alt="heraldic map of Europe" style={{pointerEvents: 'none'}} />
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+
+      <div style={{background: '#18253d', width: '100%', height: '15vh', display: 'grid', gridTemplateColumns: '2fr 1fr', alignItems: 'center', justifyContent: 'center'}}>
+        <h2 style={{fontFamily: "Eras Demi ITC", margin: '0 1rem'}}>
           Very cool map 😎👍 by <a className="App-link" href="https://twitter.com/Yunacel" target="_blank" rel="noopener noreferrer">Jekyll</a>
         </h2>
 
-        <a className="button" href="https://www.etsy.com/shop/HeraldikaJekyll" target="_blank" rel="noopener noreferrer">
+        <a className="button" href="https://www.etsy.com/shop/HeraldikaJekyll" target="_blank" rel="noopener noreferrer" style={{margin: '0 1rem'}}>
           <button>Buy physical version</button>
         </a>
+      </div>
 
-        <div style={{border: '0.1rem solid red', background: 'white', maxWidth: '85%', height: 'auto', margin: '5% 0 5rem'}}>
-          <TransformWrapper maxScale={6} smooth={false} wheel={wheelOpt}>
-            <TransformComponent>
-              <img src={mapPath} alt="heraldic map of Europe" style={{maxWidth: '100%', height: 'auto', pointerEvents: 'none'}} />
-            </TransformComponent>
-          </TransformWrapper>
-        </div>
-
-      </header>
     </div>
   );
 }
