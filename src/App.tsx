e5424ce8515e0eb1cce53ac852dcc7d5bad59d42
vwaki <viniwte@gmail.com>
2023-07-21 18:13:40 -0300

    Website v2, new design and mobile compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,25 @@ const wheelOpt = {step: 1.0, smoothStep: 0.005};
 function App() {
   return (
     <div className="App">
+      
+      <p style={{fontFamily: "Eras Demi ITC", fontSize: '2rem', color: '#18253d', position: 'absolute', top: '0'}}>
+        HERALDIC MAP OF EUROPE
+      </p>
+      
+      <p style={{fontFamily: "Eras Demi ITC", fontSize: '1rem', color: '#18253d', position: 'absolute', top: '78vh'}}>
+        Scroll or pinch to zoom
+      </p>
 
-      <div style={{background: 'white', maxHeight: '85%', width: 'auto', justifyContent: 'center'}}>
+      <div style={{width: '100%', height: '85%', justifyContent: 'center', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+          
         <TransformWrapper maxScale={6} smooth={true} wheel={wheelOpt} centerOnInit={true}> 
           <TransformComponent>
-            <img src={mapPath} alt="heraldic map of Europe" style={{pointerEvents: 'none'}} />
+            <img src={mapPath} alt="heraldic map of Europe" style={{pointerEvents: 'none', width: '100%', height: '85vh', objectFit: 'contain'}} />
           </TransformComponent>
         </TransformWrapper>
       </div>
 
-      <div style={{background: '#18253d', width: '100%', height: '15vh', display: 'grid', gridTemplateColumns: '2fr 1fr', alignItems: 'center', justifyContent: 'center'}}>
+      <div style={{background: '#18253d', width: '100%', height: '15%', display: 'grid', gridTemplateColumns: '2fr 1fr', alignItems: 'center', justifyContent: 'center'}}>
         <h2 style={{fontFamily: "Eras Demi ITC", margin: '0 1rem'}}>
           Very cool map 😎👍 by <a className="App-link" href="https://twitter.com/Yunacel" target="_blank" rel="noopener noreferrer">Jekyll</a>
         </h2>
